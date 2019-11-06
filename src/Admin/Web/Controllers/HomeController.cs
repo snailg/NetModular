efba8c1ab5d1640c4f5e4b493699cf1d79884ab2
oldli <wenju1991@163.com>
2019-11-06 22:25:42 +0800

    demo

---
+++ src/Admin/Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetModular.Lib.Module.AspNetCore.Attributes;
+
+namespace NetModular.Module.Admin.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/home")]
+        [DisableAuditing]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
